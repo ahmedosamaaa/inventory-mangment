722d4266387783ec99efb47408d1e871fb5057ff
Ahmed <ahmedosama12313@gmail.com>
2024-10-23 06:20:39 +0300

    dashboard stats page and its components

---
+++ client/src/app/dashboard/CardPopularProducts.tsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Rating from "@/app/_components/Rating";
+import { ShoppingBag } from "lucide-react";
+
+import { useGetDashboardMetricsQuery } from "@/state/api";
+
+const CardPopularProducts = () => {
+    const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+
+    return (
+        <div className="row-span-3 xl:row-span-6 overflow-y-scroll bg-white shadow-md rounded-2xl pb-16">
+            {isLoading ? (
+                <div className="m-5">Loading...</div>
+            ) : (
+                <div>
+                    <>
+                        <h2 className="font-semibold text-lg px-7 pt-5 mb-2">
+                            Popular Products
+                        </h2>
+                        <hr className="border-gray-200" />
+                        <div className="overflow-auto h-full">
+                            {dashboardMetrics?.popularProducts.map(
+                                (product) => (
+                                    <div
+                                        key={product.productId}
+                                        className="flex items-center justify-between gap-3 px-5 py-7 border-b border-gray-200"
+                                    >
+                                        <div className="flex items-center gap-3">
+                                            <div>img</div>
+                                            <div className="flex flex-col justify-between gap-1">
+                                                <div className="font-bold text-gray-700">
+                                                    {product.name}
+                                                </div>
+                                                <div className="flex text-sm items-center">
+                                                    <span className="font-bold text-blue-500 text-xs">
+                                                        ${product.price}
+                                                    </span>
+                                                    <span className="mx-2">
+                                                        |
+                                                    </span>
+                                                    <Rating
+                                                        rating={
+                                                            product.rating || 0
+                                                        }
+                                                    />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div className="text-sm flex items-center">
+                                            <button className="p-2 rounded-full bg-blue-100 text-blue-600 mr-2">
+                                                <ShoppingBag className="w-4 h-4" />
+                                            </button>
+                                            {Math.round(
+                                                product.stockQuantity / 1000
+                                            )}
+                                            k Sold
+                                        </div>
+                                    </div>
+                                )
+                            )}
+                        </div>
+                        <hr className="border-gray-200" />
+                    </>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default CardPopularProducts;
